cc6283690b6cfe36382d3bf4c630ce0d7a4779da
nayfaan <luknnn129@gmail.com>
2021-10-22 19:13:19 -0400

    video?

--- main.py
+++ main.py
@@ -2,38 +2,62 @@ import tkinter as tk
 from PIL import Image
 import glob
 import os
+import cv2
 
 def main():
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     input_dir = os.path.join(ROOT_DIR, "input/")
-    input_list = glob.glob(os.path.join(input_dir, "input.*"))
+    
     pixel_size = 1
-    scale = 30
+    scale = 100
     
+    '''input_list = glob.glob(os.path.join(input_dir, "*.jpg"))    
     file = Image.open(input_list[0])
-    width, height = file.size
+    width, height = file.size'''
     
-    file = file.resize((int(width/scale), int(height/scale)))#debug
-    width, height = file.size#debug'''
-    pix = file.load()
-
-    
+    input_list = glob.glob(os.path.join(input_dir, "*.mp4"))
+    cap = cv2.VideoCapture(input_list[0])
     
     window = tk.Tk()
     
-    for x in range(width):
-        for y in range(height):
-            rgb = pix[x,y]
-            frame = tk.Frame(
-                master=window,
-                borderwidth=0
-            )
-            frame.grid(row=y, column=x)
-            label = tk.Frame(master=frame, width=pixel_size, height=pixel_size)
-            label.configure(bg=f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}')
-            label.pack()
+    test = 0
     
-    window.mainloop()
+    while(1):
+        ret, frame = cap.read()
+        if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+    
+        test += 1
+        test %= 256
+    
+        '''file = file.resize((int(width/scale), int(height/scale)))#debug
+        width, height = file.size#debug'''
+        #pix = file.load()
+        
+        height = len(frame)
+        width = len(frame[0])
+        
+        
+        
+        for x in range(width)[::scale]:
+            for y in range(1,height)[::scale]:
+                #rgb = pix[x,y]
+                rgb = [test,test,test]#frame[x][y]
+
+                frame = tk.Frame(
+                    master=window,
+                    borderwidth=0
+                )
+                frame.grid(row=y, column=x)
+                label = tk.Frame(master=frame, width=pixel_size, height=pixel_size)
+                label.configure(bg=f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}')
+                label.pack()
+        
+        print("frame:",test)
+        window.mainloop()
+        window.destroy()
 
 if __name__ == '__main__':
     main()
